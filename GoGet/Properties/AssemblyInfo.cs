e3122b6dea1439b5402d3e75148107ddbadfbf8b
James Demeuse <james.demeuse@gmail.com>
2015-07-07 10:59:00 -0500

    4.5.1 to 4.5

--- GoGet/Properties/AssemblyInfo.cs
+++ GoGet/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("GoGet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("OrgName")]
+[assembly: AssemblyCompany("James Demeuse")]
 [assembly: AssemblyProduct("GoGet")]
 [assembly: AssemblyCopyright("Copyright © OrgName 2015")]
 [assembly: AssemblyTrademark("")]
@@ -40,5 +40,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
